a837253d39705915702cfd49a0f20be057d1ada3
FumiMelvinDev <fumimelvindev@gmail.com>
2024-02-27 23:16:39 +0200

    navbar and hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
+function Hero() {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12 mt-20">
+        <div className="col-span-7 place-self-center text-center lg:text-left">
+          <h1>
+            Hello, I'm{" "}
+            <span>
+              <TypeAnimation
+                sequence={["Fumi Melvin", 1200, "a Full Stack Developer", 1200]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            dignissimos praesentium nesciunt ab ut officiis velit laboriosam
+            dolores, iste voluptates, est.
+          </p>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full w-[275px] h-[275px] lg:w-[400px] lg:h-[400px] relative bg-slate-500">
+            <Image
+              src="/hero.png"
+              height={300}
+              width={300}
+              alt="Hero Image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navLinks = [
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "Projects",
+    href: "#projects",
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 p-4 bg-[#030c0c] bg-opacity-95">
+      <div className="flex flex-wrap items-center justify-between text-white">
+        <div>
+          <Link href={"/"}>Logo</Link>
+        </div>
+        <div className="mobile block md:hidden">
+          {!isOpen ? (
+            <button
+              onClick={() => setIsOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white"
+            >
+              <Bars3Icon className="h-5 w-4" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setIsOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white"
+            >
+              <XMarkIcon className="h-5 w-4" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="nav">
+          <ul className="flex flex-row space-x-6">
+            {navLinks.map((item) => (
+              <Link key={item.name} href={item.href}>
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {isOpen ? (
+        <ul className="flex flex-col md:hidden text-white">
+          {navLinks.map((item) => (
+            <Link key={item.name} href={item.href}>
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      ) : null}
+    </nav>
+  );
+}
+
+export default Navbar;
